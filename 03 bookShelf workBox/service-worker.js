9d27e8f5c4d5a8a3d8a5337fb3954988a9bd4287
dhonadwi <dhonadwi2@gmail.com>
2021-04-16 15:25:08 +0700

    workbox v-1

--- 03 bookShelf workBox/service-worker.js
+++ 03 bookShelf workBox/service-worker.js
@@ -1,71 +1,89 @@
-const CACHE_NAME = "bookShelf-v1";
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/book.html",
-  "/index.js",
-  "/pages/home.html",
-  "/pages/about.html",
-  "/pages/contact.html",
-  "/css/materialize.min.css",
-  "/js/materialize.min.js",
-  "/js/nav.js",
-  "/js/api.js",
-  "/icon/icon.png",
-  "/manifest.json",
-  "/icon.ico",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
-  "http://13.229.240.223:5000/books"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
+
+workbox.precaching.precacheAndRoute([
+  // { url: '/book.html', revision: '1' },
+  { url: '/icon.ico', revision: '1' },
+  { url: '/index.html', revision: '1' },
+  { url: '/index.js', revision: '1' },
+  { url: '/manifest.json', revision: '1' },
+  { url: '/nav.html', revision: '1' },
+  { url: '/css/materialize.min.css', revision: '1' },
+  { url: '/js/api.js', revision: '1' },
+  { url: '/js/idb.js', revision: '1' },
+  { url: '/js/materialize.min.js', revision: '1' },
+  { url: '/js/nav.js', revision: '1' },
 
 
-self.addEventListener("fetch", event => {
-  const base_url = `http://13.229.240.223:5000/`;
-  if (event.request.url.indexOf(base_url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_NAME)
-        .then(cache => {
-          return fetch(event.request)
-            .then(response => {
-              cache.put(event.request.url, response.clone());
-              return response;
-            })
-        })
-    )
-  } else {
-    event.respondWith(
-      caches.match(event.request, {
-        ignoreSearch: true
-      })
-        .then(response => {
-          return response || fetch(event.request);
-        })
-    )
-  }
-})
+  // { url: '/pages/about.html', revision: '1' },
+  // { url: '/pages/contact.html', revision: '1' },
+  // { url: '/pages/home.html', revision: '1' },
+]);
 
+// workbox.routing.registerRoute(
+//   new RegExp('/pages/'),
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: 'pages'
+//   })
+// );
 
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheName != CACHE_NAME && cacheName.startsWith("bookShelf-")) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/book.html'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'book'
+  })
+)
+
+// Menyimpan cache dari CSS Google Fonts
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+// Menyimpan cache untuk file font selama 1 tahun
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  /^http:\/\/13\.229\.240\.223:5000\/books/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'api-bookshelf',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 3,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
